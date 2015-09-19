83b7bb133f3dd9e5cd31323963c9c353426d4f9f
Brian Clark <bclark8923@gmail.com>
2015-09-19 16:27:26 -0700

    update package.json

---
+++ config/env/test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  db: 'mongodb://trialmatch:1qaz2wsx3edc@dogen.mongohq.com:10092/trial-match',
+  http: {
+    port: 3001
+  },
+  logging: {
+    format: 'common'
+  },
+  app: {
+    name: 'MEAN - A Modern Stack - Test'
+  },
+  facebook: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'CONSUMER_KEY',
+    clientSecret: 'CONSUMER_SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+  },
+  github: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  linkedin: {
+    clientID: 'API_KEY',
+    clientSecret: 'SECRET_KEY',
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+  },
+  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'SERVICE_PROVIDER',
+    auth: {
+      user: 'EMAIL_ID',
+      pass: 'PASSWORD'
+    }
+  }
+};
